42ec6a8feac9377d22995ef678009d50cd0ae1cd
Alex Martin <alexbuzzbee@gmail.com>
2020-03-27 18:29:35 -0400

    Add (broken) server code with stream transport support

---
+++ chprops/__init__.py
@@ -0,0 +1,7 @@
+"""
+Python protocol library package for the Changeable Properties Protocol.
+
+Contains support for both clients and servers using TLS, TCP, and Unix-domain stream sockets for transport.
+"""
+
+__all__ = ["common", "server", "stream"]
