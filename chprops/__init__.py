48bbd565ea2bbb2583a52c681262be25437ad313
Alex Martin <alexbuzzbee@gmail.com>
2020-03-29 21:58:33 -0400

    Add client (doesn't work, but it's there)

--- chprops/__init__.py
+++ chprops/__init__.py
@@ -4,4 +4,4 @@ Python protocol library package for the Changeable Properties Protocol.
 Contains support for both clients and servers using TLS, TCP, and Unix-domain stream sockets for transport.
 """
 
-__all__ = ["common", "server", "stream"]
+__all__ = ["common", "client", "server", "stream"]
